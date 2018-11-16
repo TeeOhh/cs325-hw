0ff82d9afbd33861675d48006eb52ac775b6424d
DESKTOP-N95G2II\taylo <olsontaf@uni.edu>
2018-11-16 16:25:38 -0600

    graph-search

--- graph-search.lisp
+++ graph-search.lisp
@@ -37,12 +37,20 @@
 (defun query-search (query &optional (blists (list nil)))
   (cond ((null blists) nil)
         ((member ':not query)
-         (set-difference blists (graph-search (cdr query) blists)))
+         (get-match blists (graph-search (cdr query) blists)))
+         ;(set-difference blists (graph-search (cdr query) blists)))
         (t
          (if (null blists) nil
            (mapcan (lambda (triple)
                      (match-query query triple blists))
              *triples*)))))
+
+(defun get-match (blists not-bindings)
+  (loop for binding in blists
+      collect (loop for binding2 in not-bindings
+                  when (match-query (car binding) (car (last binding2)))
+                    collect (car binding))))
+                               
 
 (defun match-query (query triple &optional (blists (list nil)))
   (cond ((null blists) nil)
